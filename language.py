1ecf4a7a19bab7dfb1b4666bf7a2aa8e731184f9
Charan230199 <venkatasaicharan1999@msitprogram.net>
2021-11-03 08:17:31 +0530

    Compute uniform probabilities

--- language.py
+++ language.py
@@ -137,7 +137,10 @@ Parameters: list of strs
 Returns: list of floats
 '''
 def buildUniformProbs(unigrams):
-    return
+    list = []
+    for i in unigrams:
+        list.append(1/len(unigrams))
+    return list
 
 
 '''
@@ -333,19 +336,19 @@ def scatterPlot(xs, ys, labels, title):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testCountBigrams()
+    # test.testCountBigrams()
     # print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
 #     test.week1Tests()
 #     print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
 #     test.runWeek1()
 
     ## Uncomment these for Week 2 ##
-    """
+    
     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
     test.runWeek2()
-"""
+
 
     ## Uncomment these for Week 3 ##
 """
